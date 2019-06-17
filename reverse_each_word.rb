c4fdc1d775767b91459c56c9b235b29f1a5dc1a7
Lantz Warrick <betalantz@gmail.com>
2019-06-17 13:51:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(sentence)
+  rev_array = []
   array = sentence.split(' ')
-  array.each { |word| word.reverse }
-  rev_sentence = array.join(' ')
+  array.each |word| do 
+    rev_array << word.reverse
+  end  
+  rev_sentence = rev_array.join(' ')
   rev_sentence
 end
