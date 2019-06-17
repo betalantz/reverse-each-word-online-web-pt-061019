53906527588e5074af983c71a33808ca438e8379
Lantz Warrick <betalantz@gmail.com>
2019-06-17 13:54:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,17 @@
+# def reverse_each_word(sentence)
+#   rev_array = []
+#   array = sentence.split(' ')
+#   array.each do |word|
+#     rev_array << word.reverse
+#   end
+#   rev_sentence = rev_array.join(' ')
+#   rev_sentence
+# end
+
 def reverse_each_word(sentence)
   rev_array = []
   array = sentence.split(' ')
-  array.each do |word|
-    rev_array << word.reverse
-  end
-  rev_sentence = rev_array.join(' ')
+  array.collect {|word| word.reverse}
+  rev_sentence = array.join(' ')
   rev_sentence
 end
